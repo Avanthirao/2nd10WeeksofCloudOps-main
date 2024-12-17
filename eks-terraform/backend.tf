3da342c26e5b484e356d883c35754bee05c9188b
Avanthirao <avanthimirupala02@gmail.com>
2024-12-17 12:35:41 -0500

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "chikky-bucket"
+    bucket = "bucket-avanthi"
     key    = "eks/terraform.tfstate"
-    region = "us-west-2"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
